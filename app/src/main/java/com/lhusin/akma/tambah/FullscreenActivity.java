f0e9a9d896034891c1cb8e050ea41450c9c98333
maklumi <maklumi@hotmail.com>
2015-11-04 21:46:48 +0800

    dari home computer 4 nov

--- app/src/main/java/com/lhusin/akma/tambah/FullscreenActivity.java
+++ app/src/main/java/com/lhusin/akma/tambah/FullscreenActivity.java
@@ -1,28 +1,29 @@
 package com.lhusin.akma.tambah;
 
-import android.graphics.drawable.Drawable;
-import android.media.Image;
-import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
 
     TextView textViewSoalan;
     TextView textViewBetul;
@@ -40,24 +41,11 @@ public class MainActivity extends AppCompatActivity {
     String heart="";
 
 
-    @Override
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Mula!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                mulakanMain(fab);
-                fab.setVisibility(View.INVISIBLE);
-            }
-        });
-
+        setContentView(R.layout.activity_fullscreen);
 
         textViewSoalan = (TextView)findViewById(R.id.textViewSoalan);
         buttonTag1 = (Button)findViewById(R.id.buttonTag1);
@@ -71,9 +59,8 @@ public class MainActivity extends AppCompatActivity {
         textViewBetul = (TextView)findViewById(R.id.textViewBetul);
         textViewSalah = (TextView)findViewById(R.id.textViewSalah);
         heart = new String(new int[] {0x2764}, 0,1);
-
-        Log.i("heart", heart);
     }
+
 
     public void mulakanMain(View v) {
         fab.setVisibility(View.INVISIBLE);
@@ -152,14 +139,14 @@ public class MainActivity extends AppCompatActivity {
         if (v.getTag().toString().equals(Integer.toString(tagBetul))) {
             skor++;
 
-           // Toast.makeText(getApplicationContext(), "Betul", Toast.LENGTH_SHORT).show();
+            // Toast.makeText(getApplicationContext(), "Betul", Toast.LENGTH_SHORT).show();
             textViewBetul.setText(Integer.toString(skor));
 
 
         } else {
 
-          //  Toast.makeText(getApplicationContext(),"tak", Toast.LENGTH_SHORT).show();
-           // textViewSalah.setText(Integer.toString(bilanganSoalan - skor));
+            //  Toast.makeText(getApplicationContext(),"tak", Toast.LENGTH_SHORT).show();
+            // textViewSalah.setText(Integer.toString(bilanganSoalan - skor));
             if ((bilanganSoalan - skor ) < 6 ) {
                 textViewSalah.setText(new String(new char[5-(bilanganSoalan-skor)]).replace("\0", heart));
             } else {
@@ -175,31 +162,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-       @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        menu.getItem(0).setEnabled(false);
-        return super.onPrepareOptionsMenu(menu);
-    }
 }
